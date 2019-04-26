99cf411ec019869d967471a92028ba46deeef000
JichangJang <wlckd90@gmail.com>
2019-04-26 18:01:42 +0900

    url 끝에 슬래시 추가함

--- StudyWithMentor/assignment_003/src/workmanagement/urls.py
+++ StudyWithMentor/assignment_003/src/workmanagement/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('worklist/<int:pk>/update/', ListUpdateView.as_view(), name='worklist_update'),
     path('worklist/<int:pk>/delete/', ListDeleteView.as_view(), name='worklist_delete'),
     path('card/<int:fk>/add/', CardCreateView.as_view(), name='card_add'),
-    path('card/<int:fk>/<int:pk>/update', CardUpdateView.as_view(), name='card_update'),
-    path('card/<int:fk>/<int:pk>/delete', CardDeleteView.as_view(), name='card_delete'),
+    path('card/<int:fk>/<int:pk>/update/', CardUpdateView.as_view(), name='card_update'),
+    path('card/<int:fk>/<int:pk>/delete/', CardDeleteView.as_view(), name='card_delete'),
 ]
