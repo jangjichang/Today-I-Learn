8e46a4e7e8a3be64a364357c9ddea1fd7a3d0e40
jangjichang <wlckd90@gmail.com>
2019-04-28 09:38:54 +0900

    Root url로 접속시 /todo/ 로 리다이렉트함

--- StudyWithMentor/assignment_003/src/todoapp/urls.py
+++ StudyWithMentor/assignment_003/src/todoapp/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from .views import HomeRedirectView
 from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomeRedirectView.as_view()),
     path('todo/', include('workmanagement.urls')),
 ]
