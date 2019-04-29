96e7ca3a7ad092ccc639e766d36bfe1230c1d194
jangjichang <wlckd90@gmail.com>
2019-04-29 23:37:48 +0900

    회원가입 완료 view 추가

--- StudyWithMentor/assignment_003/src/todoapp/views.py
+++ StudyWithMentor/assignment_003/src/todoapp/views.py
@@ -50,3 +50,7 @@ class UserCreateView(CreateView):
     template_name = 'registration/register.html'
     form_class = UserCreationForm
     success_url = reverse_lazy('register_done')
+
+
+class UserCreateDoneTV(TemplateView):
+    template_name = 'registration/register_done.html'
