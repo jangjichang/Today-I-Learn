bd7eb9eaaf3c2a2069c3b1beb7a01c3d5e2f80f7
jangjichang <wlckd90@gmail.com>
2019-05-02 21:41:42 +0900

    admin 사이트에 Work, Card, Activity 등록

---
+++ StudyWithMentor/assignment_003_feedback/src/todolist/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Work, Card, Activity
+
+# Register your models here.
+
+admin.site.register(Work)
+admin.site.register(Card)
+admin.site.register(Activity)
