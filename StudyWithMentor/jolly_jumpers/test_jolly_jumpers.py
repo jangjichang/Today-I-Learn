eae6993b5f5a594bc414acebce029f7324d1dc08
jangjichang <wlckd90@gmail.com>
2019-05-05 01:41:46 +0900

    구현 방안 주석 추가

--- StudyWithMentor/jolly_jumpers/test_jolly_jumpers.py
+++ StudyWithMentor/jolly_jumpers/test_jolly_jumpers.py
@@ -17,6 +17,14 @@ output = "Jolly"
 #            "Not jolly",
 #            "Jolly"]
 
+"""
+구현 방안
+- 1부터 n-1까지 수가 담겨 있는 set 자료구조인 변수 set_input을 만든다.
+- 문제에서 주어진 n개의 정수가 담겨 있는 배열을 반복문을 돌면서 차를 구한다. 
+- 차를 set_input에서 pop한다.
+    - 원소가 없어서 pop을 못하는 경우, 졸리점프가 아니므로 Not jolly를 출력한다.
+"""
+
 
 def test_simple():
     assert output == jolly_jumpers(input)
