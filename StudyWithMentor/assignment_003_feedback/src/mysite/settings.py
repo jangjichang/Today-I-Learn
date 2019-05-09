7c6d91879126d3d629483e51a3ac87a51561d3e1
jangjichang <wlckd90@gmail.com>
2019-05-09 09:25:10 +0900

    수정 - 기본 언어 영어에서 한국어로 변경

--- StudyWithMentor/assignment_003_feedback/src/mysite/settings.py
+++ StudyWithMentor/assignment_003_feedback/src/mysite/settings.py
@@ -107,7 +107,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko'
 
 # INSTALLED_APPS 'crispy_forms',
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
