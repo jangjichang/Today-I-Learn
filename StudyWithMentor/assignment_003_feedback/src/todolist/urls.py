b284328aa87bade38f76e1746558d56fffbcbf67
jangjichang <wlckd90@gmail.com>
2019-05-09 09:47:18 +0900

    추가 - work complete url

--- StudyWithMentor/assignment_003_feedback/src/todolist/urls.py
+++ StudyWithMentor/assignment_003_feedback/src/todolist/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('work/add/', WorkCreateView.as_view(), name='work_add'),
     path('work/<int:pk>/update/', WorkUpdateView.as_view(), name='work_update'),
     path('work/<int:pk>/delete/', WorkDeleteView.as_view(), name='work_delete'),
+    path('work/<int:pk>/complete/'. WorkCompleteView.as_view(), name='work_complete'),
     path('card/<int:fk>/add/', CardCreateView.as_view(), name='card_add'),
     path('card/<int:fk>/<int:pk>/update/', CardUpdateView.as_view(), name='card_update'),
     path('card/<int:fk>/<int:pk>/delete/', CardDeleteView.as_view(), name='card_delete'),
