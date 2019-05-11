a641881e25f37b9175145afb272b4f7cd8823934
jangjichang <wlckd90@gmail.com>
2019-05-11 23:02:28 +0900

    개발, 배포 환경 분기에 따른 변수 설정 추가

--- StudyWithMentor/assignment_003_feedback/src/mysite/settings.py
+++ StudyWithMentor/assignment_003_feedback/src/mysite/settings.py
@@ -11,6 +11,21 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+try:
+    if os.environ.get("now_env") == 'dev':
+        from .local_settings import *
+    else:
+        from .dev_settings import *
+except ImportError as e:
+    print(e)
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = secret_key
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = debug
+ALLOWED_HOSTS = [allowed_host]
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -18,17 +33,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@n+o9u5hgch=&dl00a(usn(o(2r*pihjtk*t+lmqk#7#!&bl4('
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# os.environ.get('dev_debug')
-
-
-ALLOWED_HOSTS = []
-# os.environ.get('dev_debug')
 
 # Application definition
 
