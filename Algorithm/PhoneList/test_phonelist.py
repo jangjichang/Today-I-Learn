eb2b9f7892a12332b5ef60ca0121c01da5be3eaa
jangjichang <wlckd90@gmail.com>
2019-06-19 23:55:47 +0900

    chore: 문제 풀이 방법 주석 추가

--- Algorithm/PhoneList/test_phonelist.py
+++ Algorithm/PhoneList/test_phonelist.py
@@ -1,3 +1,13 @@
+"""
+원소 길이를 key, 길이에 해당하는 원소를 value로 설정
+각 key의 value들 중에 같은게 있는지 확인, 있으면 return false
+key가 가장 큰 value들을 가장 작은 key의 길이로 split하고, 그 값이 해당 key의 value에 있는지 확인 있으면 return false
+모든 작업을 마치고 return true
+
+배열의
+이를 배열의 처음부터 순회하면서 진행한다.
+"""
+
 from collections import Counter
 
 phone_book = [["119", "97674223", "1195524421"],
@@ -13,11 +23,10 @@ def test_simple():
     for index, value in enumerate(phone_book):
         assert solution(value) == result[index]
 
-
 def solution(pb):
-    c = [i[0] for i in pb]
-    c = Counter(c)
-    for key, value in c.items():
-        if c[key] >= 2:
-            return False
-    return True
+    # c = [i[0] for i in pb]
+    # c = Counter(c)
+    # for key, value in c.items():
+    #     if c[key] >= 2:
+    #         return False
+    # return True
