065b1223fc1e2641a259982d9590758a45d0a5e1
JichangJang <wlckd90@gmail.com>
2019-06-21 18:04:54 +0900

    feat: 프로그래머스 다른 사람의 답변 참고하여 작성한 코드

---
+++ Algorithm/PhoneList/test_phonelist_v2.py
@@ -0,0 +1,21 @@
+phone_book = [["119", "97674223", "1195524421"],
+              ["123", "456", "789"],
+              ["12", "123", "1235", "567", "88"]]
+
+result = [False,
+          True,
+          False]
+
+
+def test_simple():
+    for i in range(len(phone_book)):
+        assert sol(phone_book[i]) == result[i]
+
+
+def sol(pb):
+    pb = sorted(pb)
+    for key, value in enumerate(pb):
+        for values in pb[key+1:]:
+            if values.startswith(value):
+                return False
+    return True
